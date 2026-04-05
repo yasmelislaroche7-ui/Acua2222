b445d3166f6209c3140fea60e10dcfa829b75d37
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2026-04-05 00:24:07 +0000

    H

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const devOrigins = ['localhost']
-if (process.env.REPLIT_DEV_DOMAIN) {
-  devOrigins.push(process.env.REPLIT_DEV_DOMAIN)
-}
-if (process.env.REPLIT_DOMAINS) {
-  devOrigins.push(...process.env.REPLIT_DOMAINS.split(','))
-}
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -15,7 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  allowedDevOrigins: devOrigins,
 }
 
 export default nextConfig
