6cd3694021d025e22d76a454e43909e52dff4f25
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2026-04-08 16:21:14 +0000

    H

--- contracts-hh/scripts/fund.js
+++ contracts-hh/scripts/fund.js
@@ -27,13 +27,13 @@ async function main() {
   // puedes poner 1, 1000, 50000, etc
 
   const tokens = [
-    { symbol: "H2O",   address: "0x17392e5483983945dEB92e0518a8F2C4eB6bA59d", amount: "5000" },
-    { symbol: "WCOP",  address: "0x8a1d45e102e886510e891d2ec656a708991e2d76", amount: "50" },
-    { symbol: "WARS",  address: "0x0dc4f92879b7670e5f4e4e6e3c801d229129d90d", amount: "40" },
-    { symbol: "FIRE",  address: "0x22c40632c13a7f3cae9c343480607d886832c686", amount: "10000" },
-    { symbol: "BTCH2O",address: "0xEcC4dAe4DC3D359a93046bd944e9ee3421A6A484", amount: "100000" },
-    { symbol: "WLD",   address: "0x2cFc85d8E48F8EAB294be644d9E25C3030863003", amount: "3" },
-    { symbol: "UTH2",  address: "0x9eA8653640E22A5b69887985BB75d496dc97022a", amount: "1000" },
+    { symbol: "H2O",   address: "0x17392e5483983945dEB92e0518a8F2C4eB6bA59d", amount: "15000" },
+    { symbol: "WCOP",  address: "0x8a1d45e102e886510e891d2ec656a708991e2d76", amount: "8000" },
+    { symbol: "WARS",  address: "0x0dc4f92879b7670e5f4e4e6e3c801d229129d90d", amount: "2000" },
+    { symbol: "FIRE",  address: "0x22c40632c13a7f3cae9c343480607d886832c686", amount: "200000" },
+    { symbol: "BTCH2O",address: "0xEcC4dAe4DC3D359a93046bd944e9ee3421A6A484", amount: "1000000" },
+    { symbol: "WLD",   address: "0x2cFc85d8E48F8EAB294be644d9E25C3030863003", amount: "2" },
+    { symbol: "UTH2",  address: "0x9eA8653640E22A5b69887985BB75d496dc97022a", amount: "0" },
   ];
 
   for (const t of tokens) {
